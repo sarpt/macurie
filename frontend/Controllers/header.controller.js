dea61252b1495e71019c2cb9e33dc0c5c58b6917
Sarpt <mike.m.info@gmail.com>
2017-06-19 10:08:49 +0200

    Modified home page, fixed paper add form

--- frontend/Controllers/header.controller.js
+++ frontend/Controllers/header.controller.js
@@ -4,6 +4,10 @@ module.exports = function ($scope, $userService) {
     $scope.userRole = $userService.getRoleType();
   });
 
+  $scope.logout = () => {
+    $userService.setLogged(false);
+  };
+
   $scope.$on('$destroy', () => {
     /* $scope reference is "passed" to $userService, however there's no guarantee header's
     $scope will always be in memory, even though it's not supposed to be constructed dynamically
